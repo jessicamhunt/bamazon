20e8dc3e68d6ddfb11df3b51808f85d6036496b3
Jessica Hunt <jessicamarilynhunt@gmail.com>
2019-08-08 02:21:13 -0400

    add for loop and console log to display items available for sale.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,38 +5,50 @@ var inquirer = require("inquirer");
 
 //store info needed to create a connection to mysql workbench database
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    port: 3306,
-  
-    user: "root",
-  
-    password: "Cobain@1989",
-    database: "bamazon"
-  });
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "Cobain@1989",
+  database: "bamazon"
+});
 
 //test connection to mysql
-  connection.connect(function(err) {
-    if (err) throw err;
-    console.log("connected as id " + connection.threadId + "\n");
-    afterConnection();
-  });
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId + "\n");
+  afterConnection();
+});
 
 //once connected, query to mysql grabbing data from the table named products
 //test for errors and end connection
-  function afterConnection() {
-    connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
-    });
-  }
-  
+function afterConnection() {
+  connection.query("SELECT * FROM products", function (err, res) {
+    
+    if (err) throw err;
+    console.log(res);
+    console.log("-----------------------------------");
+    connection.end();
+
+    for (var i = 0; i < res.length; i++) {
+
+      console.log(res[i].id + " | " + res[i].product + " | " + res[i].department + " | " + res[i].price + " | " + res[i].quantity);
+    }
+
+    console.log("-----------------------------------");
+
+  });
+}
+
 //display items available for sale
 
 //prompt with two messages
-    //ask for id of product
-    //how many units of product (quantity)
+
+//ask for id of product
+
+//how many units of product (quantity)
 
 //place customer order
 
@@ -47,6 +59,3 @@ var connection = mysql.createConnection({
 //update sql database to reflect remaining quantity
 
 //after update, show customer the total cost of order
-
-
-
